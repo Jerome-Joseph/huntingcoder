5d0ef8d84cb52fbce2a390eeb0db838c4e30bb13
Jerome-Joseph <jeromejoseph782@gmail.com>
2022-06-18 23:32:44 +0530

    commit 3

--- pages/blogposts/[slug].js
+++ pages/blogposts/[slug].js
@@ -1,11 +1,19 @@
 import React from 'react'
 import {useRouter} from 'next/router'
+import styles from '../../styles/BlogPost.module.css'
 
 const slug = () => {
     const router = useRouter();
     const {slug} = router.query;
   return (
-    <div>{slug}</div>
+    <div className={styles.container}>
+      <main className={styles.main}>
+      <h1>Title of the page - {slug}</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum dolore aperiam fugiat odit officia ullam dignissimos rerum nesciunt deserunt molestias eos, alias asperiores nam qui quam accusamus nostrum laboriosam eaque repellendus ipsum, cumque autem maxime accusantium! Fugiat architecto eligendi earum eius. Assumenda quibusdam ipsum doloribus minima excepturi nihil, earum alias!
+      </div>
+      </main>
+    </div>
   )
 }
 
--- pages/blogs.js
+++ pages/blogs.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import Link from 'next/link'
+import styles from '../styles/Blog.module.css'
 
 const Blogs = () => {
   return (
-    <div>This is Blogs Homepage</div>
+    <div className={styles.container}>
+      <main className={styles.main}>
+          <div className="blogItems">
+            <Link href={`/blogposts/learn-javascript`}>
+            <h3 className={styles.blogh3}>How to Learn Javascript in 2022?</h3></Link>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          </div>
+          <div className="blogItems">
+            <h3>How to Learn Javascript in 2022?</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          </div>
+          <div className="blogItems">
+            <h3>How to Learn Javascript in 2022?</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          </div>
+          <div className="blogItems">
+            <h3>How to Learn Javascript in 2022?</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          </div>
+      </main>
+    </div>
   )
 }
 
